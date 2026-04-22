6a90297bdb538a9954074e437136a02f01d6cc37
Eduardo Cassiano <eduardovankepler@gmail.com>
2026-04-22 10:19:18 -0300

    refactor: extract registry domain helpers from store

--- backend/src/data/store.js
+++ backend/src/data/store.js
@@ -36,8 +36,19 @@ import {
   buildDevelopmentRecordAuditDetail
 } from "./storeGrowthDomain.js";
 import {
+  assertNoDuplicatePersonProfile,
+  assertNoManagerCycle,
+  assertValidAreaManagerReference,
+  assertValidAreaReference,
+  assertValidManagerReference,
+  assignAreaLeadershipSnapshot,
   buildAreaAuditDetail,
   buildPersonAuditDetail,
+  enrichArea,
+  enrichPerson,
+  filterAreasForUser,
+  filterPeopleForUser,
+  getTeamPeople,
   prepareAreaMutation,
   prepareCompetencyMutation,
   preparePersonMutation
@@ -137,51 +148,6 @@ function presentCompetency(competency) {
   };
 }
 
-function enrichArea(people, area) {
-  const manager = people.find((item) => item.id === area.managerPersonId);
-  const members = people.filter((person) => person.area === area.name);
-  return {
-    ...area,
-    managerPersonId: area.managerPersonId || null,
-    managerName: manager?.name || "",
-    peopleCount: members.length
-  };
-}
-
-function filterAreasForUser(areas, people, user) {
-  const enrichedAreas = areas.map((area) => enrichArea(people, area));
-
-  if (isOrgWideUser(user) || isHrUser(user) || isAdminUser(user)) {
-    return enrichedAreas;
-  }
-
-  if (isManagerUser(user)) {
-    return enrichedAreas.filter(
-      (area) => area.managerPersonId === user.person.id || area.name === user.person.area
-    );
-  }
-
-  return enrichedAreas.filter((area) => area.name === user?.person?.area);
-}
-
-function enrichPerson(people, person, areas = []) {
-  const manager = people.find((item) => item.id === person.managerPersonId);
-  const area = areas.find((item) => item.name === person.area);
-  return {
-    ...person,
-    workUnit: String(person.workUnit || DEFAULT_WORK_UNIT),
-    workMode: normalizeWorkMode(person.workMode),
-    managerPersonId: person.managerPersonId || null,
-    managerName: manager?.name || "",
-    areaManagerPersonId: area?.managerPersonId || null,
-    areaManagerName: area?.managerName || ""
-  };
-}
-
-function getTeamPeople(people, managerPersonId) {
-  return people.filter((person) => person.managerPersonId === managerPersonId);
-}
-
 function toPublicUser(db, user) {
   const person = db.people.find((item) => item.id === user.personId);
   const publicPerson = person ? enrichPerson(db.people, person) : null;
@@ -226,56 +192,6 @@ function toAdminUserRow(db, user) {
   };
 }
 
-function filterPeopleForUser(people, user, areas = []) {
-  const enrichedAreas = areas.map((area) => enrichArea(people, area));
-  const enrichedPeople = people.map((person) => enrichPerson(people, person, enrichedAreas));
-
-  if (isAdminUser(user) || isHrUser(user)) {
-    return enrichedPeople;
-  }
-
-  if (isManagerUser(user)) {
-    return enrichedPeople.filter(
-      (person) => person.id === user.person.id || person.managerPersonId === user.person.id
-    );
-  }
-
-  return enrichedPeople.map((person) => ({
-    id: person.id,
-    name: person.name,
-    roleTitle: person.roleTitle,
-    area: person.area,
-    workUnit: person.workUnit,
-    workMode: person.workMode,
-    managerPersonId: person.managerPersonId,
-    managerName: person.managerName
-  }));
-}
-
-function assignAreaLeadershipSnapshot(areas, personId, areaName, shouldLeadArea) {
-  const nextAreaKey = normalizeAreaName(areaName);
-
-  return areas.map((area) => {
-    const isTargetArea = normalizeAreaName(area.name) === nextAreaKey;
-    const isManagedByPerson = area.managerPersonId === personId;
-
-    if (isManagedByPerson && (!shouldLeadArea || !isTargetArea)) {
-      return {
-        ...area,
-        managerPersonId: null
-      };
-    }
-
-    if (shouldLeadArea && isTargetArea) {
-      return {
-        ...area,
-        managerPersonId: personId
-      };
-    }
-
-    return area;
-  });
-}
 
 function isSameWorkUnit(leftPerson, rightPerson) {
   return (
@@ -874,79 +790,6 @@ function presentDelinquentAssignment(assignment, db, cycleStatus) {
     daysOverdue: calculateDaysOverdue(assignment.dueDate),
     isDelinquent: isAssignmentDelinquent(assignment, cycleStatus)
   };
-}
-
-function assertValidManagerReference(people, managerPersonId, personId = null) {
-  if (!managerPersonId) {
-    return;
-  }
-
-  const managerExists = people.some((person) => person.id === managerPersonId);
-  if (!managerExists) {
-    throw new Error("Gestor informado nao foi encontrado.");
-  }
-
-  if (personId && managerPersonId === personId) {
-    throw new Error("Uma pessoa nao pode ser gestora de si mesma.");
-  }
-}
-
-function assertNoManagerCycle(people, managerPersonId, personId = null) {
-  if (!managerPersonId || !personId) {
-    return;
-  }
-
-  let currentManagerId = managerPersonId;
-  const visited = new Set();
-
-  while (currentManagerId && !visited.has(currentManagerId)) {
-    if (currentManagerId === personId) {
-      throw new Error("A hierarquia informada cria um ciclo de gestao invalido.");
-    }
-
-    visited.add(currentManagerId);
-    currentManagerId =
-      people.find((person) => person.id === currentManagerId)?.managerPersonId || null;
-  }
-}
-
-function assertValidAreaReference(areas, areaName) {
-  if (!areaName || !areas.some((area) => normalizeAreaName(area.name) === normalizeAreaName(areaName))) {
-    throw new Error("Area informada nao foi encontrada.");
-  }
-}
-
-function assertValidAreaManagerReference(people, managerPersonId) {
-  if (!managerPersonId) {
-    return;
-  }
-
-  const managerExists = people.some((person) => person.id === managerPersonId);
-  if (!managerExists) {
-    throw new Error("Responsavel da area nao foi encontrado.");
-  }
-}
-
-function assertNoDuplicatePersonProfile(people, payload, personId = null) {
-  const normalizedName = normalizeOptionalText(payload.name).toLowerCase();
-  const normalizedArea = normalizeAreaName(payload.area);
-  const normalizedRoleTitle = normalizeOptionalText(payload.roleTitle).toLowerCase();
-
-  const duplicate = people.find((person) => {
-    if (personId && person.id === personId) {
-      return false;
-    }
-
-    return (
-      normalizeOptionalText(person.name).toLowerCase() === normalizedName &&
-      normalizeAreaName(person.area) === normalizedArea &&
-      normalizeOptionalText(person.roleTitle).toLowerCase() === normalizedRoleTitle
-    );
-  });
-
-  if (duplicate) {
-    throw new Error("Ja existe uma pessoa com mesmo nome, area e cargo.");
-  }
 }
 
 function enrichIncident(incident, people = [], areas = []) {
